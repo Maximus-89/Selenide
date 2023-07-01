aee9787c3843fc957b1c1e7e5e45206def49a879
Maximus <maxis30.09@mail.ru>
2023-07-01 17:05:37 +0300

    Success

---
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -0,0 +1,41 @@
+package ru.netology;
+
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTest {
+    public static String setLocalDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy",
+                new Locale("ru")));
+    }
+
+    @BeforeEach
+    void setupTest() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSubmitValidData3() {
+        String date = setLocalDate(3);
+        $("[data-test-id=city] .input__control").setValue("Ставрополь");
+        $("[data-test-id = date] .input__control").doubleClick().sendKeys(date);
+        $("[data-test-id = name] .input__control").setValue("Крепышев Василий");
+        $("[data-test-id = phone] .input__control").setValue("+79200000000");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $("[data-test-id = notification]").shouldHave(text("Успешно!"),
+                Duration.ofSeconds(15)).shouldBe(visible);
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                Duration.ofSeconds(15)).shouldBe(visible);
+    }
+}
+
+
